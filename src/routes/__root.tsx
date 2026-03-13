38af0d69a174eaf334d3a8bb50a5463e99e7078c
Bakytzhan Izbasarov <bakytzanizbasarov77@gmail.com>
2026-03-13 12:47:52 +0500

    2. Корневой layout, Outlet и навигация с Link

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,13 +5,26 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <div style={{ display: "flex", gap: "10px", padding: "10px" }}>
-        <Link to="/" className="[&.active]:font-bold">
+        <Link
+          to="/"
+          activeOptions={{ exact: true }}
+          activeProps={{ style: { fontWeight: "bold", color: "blue" } }}
+          inactiveProps={{ style: { color: "gray" } }}
+        >
           Home
         </Link>
-        <Link to="/about" className="[&.active]:font-bold">
+        <Link
+          to="/about"
+          activeProps={{ style: { fontWeight: "bold", color: "blue" } }}
+          inactiveProps={{ style: { color: "gray" } }}
+        >
           About
         </Link>
-        <Link to="/contacts" className="[&.active]:font-bold">
+        <Link
+          to="/contacts"
+          activeProps={{ style: { fontWeight: "bold", color: "blue" } }}
+          inactiveProps={{ style: { color: "gray" } }}
+        >
           Contacts
         </Link>
       </div>
