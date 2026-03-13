d4db44e38025390c7fd56d5cb7a71df144d0d46c
Bakytzhan Izbasarov <bakytzanizbasarov77@gmail.com>
2026-03-13 19:08:07 +0500

    15. Code splitting — autoCodeSplitting в Vite плагине

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tanstackRouter(), react()],
+  plugins: [tanstackRouter({ autoCodeSplitting: true }), react()],
 });
